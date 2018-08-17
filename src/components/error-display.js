f654ffcdf8926bdda57d9e55723f98035f12e1ab
patersog <gianluca.paterson@gmail.com>
2018-08-17 18:08:06 -0400

    Update error-display, Refactor/styles

--- src/components/error-display.js
+++ src/components/error-display.js
@@ -4,16 +4,18 @@ import './styles/error-display.css';
 
 export function ErrorDisplay() {
 	return(
-		<section className="error-display">
-			<div className="error-container">
-				<h2 className="error-header">
-				Oops! Something went wrong!
-				</h2>
-				<div className="error-main">
-					<p><span><strong>500 Internal Server Error</strong></span>
-						<br/>
-					Please try loading the page again.
-					</p>
+		<section className="sec error">
+			<div className="inner ivory">
+				<div className="container">
+					<div className="server-error">
+						<h2 className="error-header">
+					Oops! Something went wrong!
+						</h2>
+						<p><span className="error-message"><strong>500 Internal Server Error</strong></span>
+							<br/>
+						Please try loading the page again.
+						</p>
+					</div>
 				</div>
 			</div>
 		</section>
